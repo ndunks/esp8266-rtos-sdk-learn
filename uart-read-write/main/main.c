ae6b6b35357ebcd969944daaa4d8df46989c0d95
Mochamad Arifin <klampok.child@gmail.com>
2021-10-24 23:02:10 +0700

    Uart Read Write

--- uart-read-write/main/main.c
+++ uart-read-write/main/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "driver/uart.h"
 
 #define BUF_SIZE (1024)
+#define BUF_SIZE (1024)
+#define RD_BUF_SIZE (BUF_SIZE)
+static QueueHandle_t uart0_queue;
+static const char *TAG = "APP";
+static volatile uint delayMs = 200U;
 
-volatile uint delayMs = 200U;
+void configure_uart(){
+    // Install UART driver, and get the queue.
+    uart_driver_install(CONFIG_CONSOLE_UART_NUM, BUF_SIZE * 2, BUF_SIZE * 2, 100, &uart0_queue, 0);
+}
 
 void configure_pins()
 {
@@ -32,8 +43,8 @@ void blink_task(void *null)
     {
         // Delay and turn on
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        gpio_set_level(GPIO_NUM_2, state);
-        state ^= 1;
+        gpio_set_level(GPIO_NUM_2, state ^= 1);
+        printf("BLINK %d\n", state);
     }
     vTaskDelete(NULL);
 }
@@ -43,27 +54,64 @@ void blink_task(void *null)
  */
 void command_task(void *null)
 {
-    // Configure parameters of an UART driver,
-    // communication pins and install the driver
-    uart_config_t uart_config = {
-        .baud_rate = CONFIG_ESPTOOLPY_MONITOR_BAUD,
-        .data_bits = UART_DATA_8_BITS,
-        .parity    = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
-    };
-    uart_param_config(UART_NUM_0, &uart_config);
-    uart_driver_install(UART_NUM_0, BUF_SIZE * 2, 0, 0, NULL, 0);
-    // Configure a temporary buffer for the incoming data
-    uint8_t *buffer = (uint8_t *)malloc(BUF_SIZE);
-    while (1)
-    {
-        // // Read data from the UART
-        int len = uart_read_bytes(UART_NUM_0, buffer, BUF_SIZE, 20 / portTICK_PERIOD_MS);
-        // // Write data back to the UART
-        uart_write_bytes(UART_NUM_0, (const char *)buffer, len);
-        //
+     uart_event_t event;
+    uint8_t *dtmp = (uint8_t *) malloc(RD_BUF_SIZE);
+
+    for (;;) {
+        // Waiting for UART event.
+        if (xQueueReceive(uart0_queue, (void *)&event, (portTickType)portMAX_DELAY)) {
+            bzero(dtmp, RD_BUF_SIZE);
+            ESP_LOGI(TAG, "uart[%d] event:", CONFIG_CONSOLE_UART_NUM);
+
+            switch (event.type) {
+                // Event of UART receving data
+                // We'd better handler data event fast, there would be much more data events than
+                // other types of events. If we take too much time on data event, the queue might be full.
+                case UART_DATA:
+                    ESP_LOGI(TAG, "[UART DATA]: %d", event.size);
+                    uart_read_bytes(CONFIG_CONSOLE_UART_NUM, dtmp, event.size, portMAX_DELAY);
+                    ESP_LOGI(TAG, "[DATA EVT]:");
+                    uart_write_bytes(CONFIG_CONSOLE_UART_NUM, (const char *) dtmp, event.size);
+                    break;
+
+                // Event of HW FIFO overflow detected
+                case UART_FIFO_OVF:
+                    ESP_LOGI(TAG, "hw fifo overflow");
+                    // If fifo overflow happened, you should consider adding flow control for your application.
+                    // The ISR has already reset the rx FIFO,
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(CONFIG_CONSOLE_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+
+                // Event of UART ring buffer full
+                case UART_BUFFER_FULL:
+                    ESP_LOGI(TAG, "ring buffer full");
+                    // If buffer full happened, you should consider encreasing your buffer size
+                    // As an example, we directly flush the rx buffer here in order to read more data.
+                    uart_flush_input(CONFIG_CONSOLE_UART_NUM);
+                    xQueueReset(uart0_queue);
+                    break;
+
+                case UART_PARITY_ERR:
+                    ESP_LOGI(TAG, "uart parity error");
+                    break;
+
+                // Event of UART frame error
+                case UART_FRAME_ERR:
+                    ESP_LOGI(TAG, "uart frame error");
+                    break;
+
+                // Others
+                default:
+                    ESP_LOGI(TAG, "uart event type: %d", event.type);
+                    break;
+            }
+        }
     }
+
+    free(dtmp);
+    dtmp = NULL;
     vTaskDelete(NULL);
 }
 
@@ -71,8 +119,8 @@ void app_main()
 {
 
     configure_pins();
+    configure_uart();
     printf("STDIN %d, STDOUT %d, STDERR %d\n", stdin->_file, stdout->_file, stderr->_file);
-
-    xTaskCreate(&blink_task, "blink", 158, NULL, 1, NULL);
+    xTaskCreate(&blink_task, "blink", 1000, NULL, 1, NULL);
     xTaskCreate(&command_task, "command", 1000, NULL, 2, NULL);
 }
