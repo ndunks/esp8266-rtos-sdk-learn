e9365fe37e4a2cbaf473854812ed1884501cfcb2
Mochamad Arifin <klampok.child@gmail.com>
2021-10-25 20:08:04 +0700

    Add skeleton

---
+++ _skeleton_template/main/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "main.h"
+
+static const char *TAG = "APP";
+
+void __attribute__((weak)) app_main()
+{
+    ESP_LOGI(TAG, "App Started\n");
+
+    // xTaskCreate(&mytask, "my", 5000, NULL, 2, NULL);
+}
